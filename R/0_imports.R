4374450a5204257cdd14d95a60b75135d770f8bb
Quang Nguyen <quangpmnguyen@gmail.com>
2022-09-11 15:28:52 -0400

    Initial commit

---
+++ R/0_imports.R
@@ -0,0 +1,6 @@
+# Import script
+
+#' @importFrom parsnip set_new_model
+#' @importFrom stats lm
+#' @importFrom MASS stepAIC
+#' @importFrom nnet multinom
---
+++ R/parsnip_prelims.R
@@ -0,0 +1,3 @@
+# This file host the `make_xxx()` function that registers
+# stepwise regression models
+# nocov
